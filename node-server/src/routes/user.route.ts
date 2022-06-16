b833784902f9e46919c20c2aa66a0291e3097b9c
emadmhmd <emad.mohamedeng@gmail.com>
2022-06-16 15:28:28 +0200

    Clear user.route.ts

--- node-server/src/routes/user.route.ts
+++ node-server/src/routes/user.route.ts
@@ -3,8 +3,6 @@ import userController from '../controllers/user.controller';
 
 const router = express.Router();
 
-router.post('/signup', userController.addUser); // test add to alias
-
-// router.post('/testOcr',ocrController.testOcr);
+router.post('/signup', userController.addUser);
 
 export default router;
