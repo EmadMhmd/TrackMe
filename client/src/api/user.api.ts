d7bba04f2b9b8b8e797de6f566a29557653af7dd
emadmhmd <emad.mohamedeng@gmail.com>
2022-06-16 15:28:53 +0200

    Rename endpoint

--- client/src/api/user.api.ts
+++ client/src/api/user.api.ts
@@ -6,7 +6,7 @@ import vars from '../config/env.config';
 const backendUrl = vars.get('backendUrl');
 
 class User {
-  addUser = (user : IUser) => axios.post(`${backendUrl}/addUser`, user);
+  signup = (user : IUser) => axios.post(`${backendUrl}/signup`, user);
 }
 
 export default new User();
